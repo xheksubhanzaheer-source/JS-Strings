862bb157964e0539f24e11f0b8981ae6047c6811
Subhan Zaheer <xheksubhan.zaheer@gmail.com>
2026-01-04 03:14:15 +0500

    Add files via upload

---
+++ JavaScript Strings/index.js
@@ -0,0 +1,25 @@
+console.log("This is string tutorial")
+let a = "Subhan";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+console.log(a.length)
+
+let real_name = "Subhan"
+let friend = "Sheikh"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b = "Sheikh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1))
+console.log(b.slice(1, 4))
+console.log(b.replace("he" , "77"))
+console.log(b.concat(a , "Zaheer"))
+
